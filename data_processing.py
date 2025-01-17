54f84b218e1a98a0c61c9e1abfbc331c6ae033ab
dayroronoa11 <167830703+dayroronoa11@users.noreply.github.com>
2025-01-17 12:27:39 +0700

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from fetch_data import fetch_data_id, fetch_bpjs
+from fetch_data import fetch_data_id, fetch_bpjs, fetch_creds
 from datetime import datetime
 
 @st.cache_data(ttl=86400)
@@ -9,11 +9,12 @@ def fetch_combined():
     # Fetch data from both sources
     df_id = fetch_data_id()
     df_bpjs = fetch_bpjs() 
+    df_creds = fetch_creds()
 
     # Check for the presence of 'email' column
     if 'email' not in df_id.columns or 'email' not in df_bpjs.columns:
         st.error("One or both dataframes are missing the 'email' column.")
-        return None
+        return None, df_creds
 
     # Preprocess emails
     df_id['email'] = df_id['email'].str.strip().str.lower()
@@ -30,12 +31,12 @@ def fetch_combined():
     # Merge the datasets on email
     df_combined = pd.merge(df_id, df_bpjs, on='email', how='outer', suffixes=('_id', '_bpjs'))
 
-    return df_combined
+    return df_combined, df_creds
 
 def finalize_data():
-    df_combined = fetch_combined()
+    df_combined, df_creds = fetch_combined()
     if df_combined is not None:
-        return df_combined
+        return df_combined, df_creds
     else:
         st.error("Failed to fetch combined data due to missing columns.")
-        return pd.DataFrame()
+        return pd.DataFrame(), pd.DataFrame()
